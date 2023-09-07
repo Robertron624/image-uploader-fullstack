65bdea4157bb2a608b617be461308a441cfe2e6e
Roberto Ramirez <robert.624@hotmail.com>
2023-09-07 11:29:05 -0500

    first commit

---
+++ image-uploader-front/src/App.tsx
@@ -0,0 +1,22 @@
+import "./App.scss";
+
+function App() {
+
+    return (
+        <>
+            <section className="container">
+                <div className="inner">
+
+                  <h1 className="main-title">
+                    Upload your image
+                  </h1>
+                  <p className="main-subtitle">
+                    File should be Jpeg, Png,...
+                  </p>
+                </div>
+            </section>
+        </>
+    );
+}
+
+export default App;
