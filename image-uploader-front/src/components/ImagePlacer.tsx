43f5244afbd32c6a2f8b08a78ce1ed3567be0e7c
Roberto Ramirez <robert.624@hotmail.com>
2023-09-07 18:03:03 -0500

    feat: dropzone to upload image

--- image-uploader-front/src/components/ImagePlacer.tsx
+++ image-uploader-front/src/components/ImagePlacer.tsx
@@ -1,7 +1,44 @@
 import './ImagePlacer.scss'
+import Dropzone from 'react-dropzone'
+
+
+interface AcceptedFiles {
+  name: string
+  size: number
+  type: string
+  lastModified: number
+  lastModifiedDate: Date
+  webkitRelativePath: string
+}
+
+const style = {
+  height: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+}
+
+
 const ImagePlacer = () => {
   return (
-    <div className='image-placer'></div>
+    <Dropzone onDrop={(acceptedFiles: AcceptedFiles) => console.log(acceptedFiles)}>
+      {({getRootProps, getInputProps}: {getRootProps: any, getInputProps: any}) => (
+          <div className='image-placer'>
+            <div {...getRootProps({style})}>
+              <input {...getInputProps()} />
+              
+              <div className="placeholders">
+                <img src="/mountain.jfif" alt="mountains" className='placeholder-image' />
+                <p className="placeholder-text">
+                  Drag & Drop your image here
+                </p>
+
+              </div>
+            </div>
+          </div>
+        )}
+    </Dropzone>
   )
 }
 
