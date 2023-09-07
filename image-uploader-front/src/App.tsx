67f97e253e174b051bd1566d1a509138b17226d1
Roberto Ramirez <robert.624@hotmail.com>
2023-09-07 16:44:21 -0500

    feat: image uploaded preview component

--- image-uploader-front/src/App.tsx
+++ image-uploader-front/src/App.tsx
@@ -1,13 +1,13 @@
 import "./App.scss";
 import Loading from "./components/Loading";
 import MainUploader from "./components/MainUploader";
+import UploadedImage from "./components/UploadedImage";
 
 function App() {
 
     return (
         <>
             <MainUploader/>
-            <Loading/>
         </>
     );
 }
